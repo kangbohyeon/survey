065a4fd4bdd5fcbaad8c3d8031887fa05d35f2fe
kangbohyeon <qhgus535@naver.com>
2025-06-22 12:05:04 +0900

    chore : react-route-dom install

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,32 +1,33 @@
-import { useState } from 'react';
-import { Question } from './@types/question.type';
-import ProgressIndicator from './components/ProgressIndicator';
-import QuestionBox from './components/QuestionBox';
+// import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+// import { Question } from './@types/question.type';
+// import ProgressIndicator from './components/ProgressIndicator';
+// import QuestionBox from './components/QuestionBox';
 
 function App() {
-  const questions: Question[] = [
-    {
-      title: '질문1입니다.',
-      desc: '설명1입니다.',
-      type: 'text',
-      required: false,
-      // option: {},
-    },
-    {
-      title: '질문2입니다.',
-      desc: '설명2입니다.',
-      type: 'text',
-      required: false,
-      // option: {},
-    },
-  ];
+  // const questions: Question[] = [
+  //   {
+  //     title: '질문1입니다.',
+  //     desc: '설명1입니다.',
+  //     type: 'text',
+  //     required: false,
+  //     // option: {},
+  //   },
+  //   {
+  //     title: '질문2입니다.',
+  //     desc: '설명2입니다.',
+  //     type: 'text',
+  //     required: false,
+  //     // option: {},
+  //   },
+  // ];
 
-  const step = 1;
+  // const step = 0;
 
-  const [answers, setAnswers] = useState<string[]>([]);
+  // const [answers, setAnswers] = useState<string[]>([]);
   return (
     <>
-      <ProgressIndicator />
+      {/* <ProgressIndicator />
       <QuestionBox
         question={questions[step]}
         questionsLength={questions.length}
@@ -39,7 +40,11 @@ function App() {
             return newAnswers;
           });
         }}
-      />
+      /> */}
+      <Routes>
+        <Route path='/' element={null} />
+        <Route path='/2' element={null} />
+      </Routes>
     </>
   );
 }

f17ab87c8491771162337ace967e0bae0657cc00
kangbohyeon <qhgus535@naver.com>
2025-07-06 22:08:38 +0900

    feat : global handle 처리 추가

---
+++ backend-kotlin/src/main/kotlin/com/servy/user/advice/GlobalControllerAdvice.kt
@@ -0,0 +1,23 @@
+package com.servy.user.advice
+
+import com.servy.user.constants.ExceptionMessage.Companion.NOT_FOUND
+import com.servy.user.constants.ExceptionMessage.Companion.RUNTIME
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalControllerAdvice {
+
+    @ExceptionHandler(EntityNotFoundException::class)
+    fun entityNotFound(): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(NOT_FOUND)
+    }
+
+    @ExceptionHandler(RuntimeException::class)
+    fun runtime(): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(RUNTIME)
+    }
+}
---
+++ backend-kotlin/src/main/kotlin/com/servy/user/constants/ExceptionMessage.kt
@@ -0,0 +1,9 @@
+package com.servy.user.constants
+
+class ExceptionMessage {
+    companion object{
+        const val NOT_FOUND = "정보 조회하는데 실패하였습니다"
+        const val RUNTIME = "시스템 에러가 발생하였습니다"
+    }
+
+}

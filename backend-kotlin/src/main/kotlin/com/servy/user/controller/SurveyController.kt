53020b2144146960fd02527d5100f25b8cca3d60
kangbohyeon <qhgus535@naver.com>
2025-07-13 23:00:50 +0900

    feat : cors 추가 완료

--- backend-kotlin/src/main/kotlin/com/servy/user/controller/SurveyController.kt
+++ backend-kotlin/src/main/kotlin/com/servy/user/controller/SurveyController.kt
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api")
 class SurveyController(
     private val surveyService: SurveyService
 ) {
